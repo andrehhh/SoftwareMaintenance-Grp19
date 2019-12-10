94fea7de8eb4d8fe0be04c3708af2186509a9b03
Andre Hanjaya <andrehanjaya9@gmail.com>
2019-12-10 17:55:04 +0800

    GameMap successfully loads map

---
+++ DiamondHunter/src/com/neet/DiamondHunter/MapView/GameMap.java
@@ -0,0 +1,67 @@
+package com.neet.DiamondHunter.MapView;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+public class GameMap extends Application {
+    public static Stage primaryStage;
+    public static TileMap tileMap;
+    
+    public BorderPane Layout1;
+    public TilePane Layout2;
+    
+	@Override
+	public void start(Stage primaryStage) {
+		GameMap.primaryStage = primaryStage;
+        GameMap.primaryStage.setTitle("Game Map");
+        
+        Platform.setImplicitExit(false);
+
+        initializeLayout();
+        showMap();
+		primaryStage.setOnCloseRequest(event -> { Platform.setImplicitExit(true); });
+	}
+	
+    public void initializeLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(GameMap.class.getResource("/com/neet/DiamondHunter/MapView/Layout1.fxml"));
+            Layout1 = (BorderPane) loader.load();
+
+            Scene scene = new Scene(Layout1);
+            
+            primaryStage.setResizable(false);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void showMap() {
+    	tileMap = new TileMap();
+    	tileMap.loadMap("/Maps/testmap.map");
+    	tileMap.loadTile("/Tilesets/testtileset.gif");
+	    
+		Layout2 = new TilePane();
+		
+		Layout2.setPrefColumns(tileMap.cols);
+		Layout2.setPrefRows(tileMap.rows);
+		tileMap.canvas();
+		Layout2.getChildren().add(tileMap.mainCanvas);
+
+		Layout1.setCenter(Layout2);
+    }
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ DiamondHunter/src/com/neet/DiamondHunter/MapView/TileMap.java
@@ -0,0 +1,79 @@
+package com.neet.DiamondHunter.MapView;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class TileMap {
+	
+	private int tileSize = 16;
+	private int tiles;
+	public int cols, rows;
+
+	private int[][] map;
+	private int[][] tileLayout;
+
+	public Canvas mainCanvas;
+	
+	private Image tileset;
+
+	public void loadMap(String mapFile) {
+		try {
+			InputStream in = getClass().getResourceAsStream(mapFile);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+			cols = Integer.parseInt(br.readLine());
+			rows = Integer.parseInt(br.readLine());
+
+			map = new int[rows][cols];
+
+			String delims = "\\s+";
+			for(int row = 0; row < rows; row++) {
+				String line = br.readLine();
+				String[] tokens = line.split(delims);
+				for(int col = 0; col < cols; col++) {
+					map[row][col] = Integer.parseInt(tokens[col]);
+				}
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadTile(String tilesetImage) {
+		try {
+			tileset = new Image(TileMap.class.getResourceAsStream(tilesetImage));
+			tiles = (int)tileset.getWidth() / tileSize;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void canvas() {
+		mainCanvas = new Canvas(640,640);
+		tileLayout = new int[rows][cols];
+
+		for(int row = 0; row < rows; row++) {
+			for(int col = 0; col < cols; col++) {
+				if(map[row][col] == 0) continue;
+
+				int rowcol = map[row][col];
+				int r = rowcol / tiles;
+				int c = rowcol % tiles;
+
+				if (r == 0) {
+					mainCanvas.getGraphicsContext2D().drawImage(tileset, c * tileSize, 0, tileSize, tileSize, col * tileSize, row * tileSize, tileSize, tileSize);
+					tileLayout[row][col] = 0;
+				}
+				else {
+					mainCanvas.getGraphicsContext2D().drawImage(tileset, c * tileSize, tileSize, tileSize, tileSize, col * tileSize, row * tileSize, tileSize, tileSize);
+					tileLayout[row][col] = 1;
+				}
+			}
+		}
+	}
+}
